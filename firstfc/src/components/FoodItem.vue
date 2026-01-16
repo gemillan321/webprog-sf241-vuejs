6df9526b02dd3cc0c535483847914ba8660cb85c
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-01-16 05:56:42 +0000

    First Commit

---
+++ firstfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      name: 'Apples, Pineapple',
+      message: 'I like apples'
+    }
+  }
+};
+</script>
+ 
+<style></style>
