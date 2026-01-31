374746e839b6dbdb629de188d0f3eca9a376ebc1
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-01-31 14:31:04 +0800

    Added lib/supabaseClient.js

--- firstfc/src/App.vue
+++ firstfc/src/App.vue
@@ -1,36 +1,21 @@
-<template>
-  <Introduction />
-
-  <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-
-  <h1>MAY MGA UTANG</h1>
-  <utang/>
-  <utang/>
-  <utang/>
-</template>
-
 <script setup>
-import Introduction from './components/Introduction.vue'
-import FoodItem from './components/FoodItem.vue'
-import Utang from './components/Utang.vue'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+ 
+const instruments = ref([])
+ 
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+ 
+onMounted(() => {
+   getInstruments()
+})
 </script>
-
-<style>
-.FoodItem{
-  border: dashed black 1px;
-  display: inline-block;
-  margin: 10px;
-  padding: 10px;
-  background-color: rgb(52, 132, 52);
-}
-.utang {
-  background-color: rgb(132, 52, 52);
-  border: dashed black 1px;
-  display: inline-block;
-  margin: 10px;
-  padding: 10px;
-}
-</style>
+ 
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
