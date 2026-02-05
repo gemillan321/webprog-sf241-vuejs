bf1ec1114865123b4396b45f29fda8288b3cbe16
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-02-05 15:02:00 +0000

    Cleaned main.js

--- firstfc/src/main.js
+++ firstfc/src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import Utang from './components/Utang.vue'
-
-
 const app = createApp(App)
 
 app.component('comment-form', CommentForm)
