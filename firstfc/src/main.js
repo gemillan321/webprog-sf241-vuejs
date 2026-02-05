ea8428cf50d4bcd2635c3f13e26c415784e64dd7
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-02-05 15:06:23 +0000

    added the link to the new components (comments)

--- firstfc/src/main.js
+++ firstfc/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
+
+
 const app = createApp(App)
 
 app.component('comment-form', CommentForm)
